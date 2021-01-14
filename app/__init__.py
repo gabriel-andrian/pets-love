c60241e3631f53a9d7375fd807fd0cf40efce735
edugraminho <edugraminho@gmail.com>
2021-01-14 13:30:09 -0300

    Feature: Breed table and route created

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from app.views.home import bp_home
+from app.views.breed_view import bp_breed
+
+from app.models.breed_model import db, mg, ma
+
 from environs import Env
 
 
@@ -13,6 +17,12 @@ def create_app():
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = env.bool("SQLALCHEMY_TRACK_MODIFICATIONS")
     app.config["SQLALCHEMY_DATABASE_URI"] = env.str("SQLALCHEMY_DATABASE_URI")
 
+    db.init_app(app)
+    mg.init_app(app, db)
+    ma.init_app(app)
+
     app.register_blueprint(bp_home)
+    app.register_blueprint(bp_breed)
+
 
     return app
---
+++ app/models/breed_model.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
+from marshmallow import fields
+
+db = SQLAlchemy()
+mg = Migrate()
+ma = Marshmallow()
+
+
+class Breed(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+
+
+class BreedSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Breed
+
+    id = ma.auto_field() 
+    name = ma.auto_field() 
