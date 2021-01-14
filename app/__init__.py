932a8b1dd3674ecb26257dae40b71be1e3050bec
Atauã Doederlein <ataua.doederlein@gmail.com>
2021-01-14 21:59:33 +0000

    Feature/authentication

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
+from secrets import token_hex
 from app.views.home import bp_home
 from app.views.owner_views import bp_owner
 from environs import Env
+from app.views.authorization_view import bp_authorization
 from app.models import db, ma, mg
 
 
@@ -15,12 +18,16 @@ def create_app():
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = env.bool(
         "SQLALCHEMY_TRACK_MODIFICATIONS")
     app.config["SQLALCHEMY_DATABASE_URI"] = env.str("SQLALCHEMY_DATABASE_URI")
+    app.config['JWT_SECRET_KEY'] = token_hex(16)
+
+    JWTManager(app)
 
     db.init_app(app)
     mg.init_app(app, db)
     ma.init_app(app)
 
     app.register_blueprint(bp_home)
+    app.register_blueprint(bp_authorization)
     app.register_blueprint(bp_owner)
 
     return app
