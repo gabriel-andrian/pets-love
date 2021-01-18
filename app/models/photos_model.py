2daa3c919e134dae18afd7d587c8069c910651e4
Gabriel Vieira Andrian <gabriel_andrian@hotmail.com>
2021-01-18 14:00:22 -0300

    fix: photo model Column changes

--- app/models/photos_model.py
+++ app/models/photos_model.py
@@ -4,10 +4,7 @@ from app.models import db, ma
 class Photo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     dog_account_id = db.Column(db.Integer,  db.foreignKey('dog.id'))
-    link = db.Column(db.Text, nullable=False)
-    details = db.Column(db.Text, nullable=True)
-    time_added = db.Column(db.DateTime(), nullable=False)
-    active = db.Column(db.Boolean, nullable=False)
+    link = db.Column(db.string, nullable=False)
 
     def __repr__(self):
         return f"<Photo {self.id} />"
@@ -20,6 +17,3 @@ class PhotoSchema(ma.SQLAlchemySchema):
     id = ma.auto_field()
     dog_account_id = ma.auto_field()
     link = ma.auto_field()
-    details = ma.auto_field()
-    time_added = ma.auto_field()
-    active = ma.auto_field()
