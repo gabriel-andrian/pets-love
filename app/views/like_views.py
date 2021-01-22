e8b0bb4ff3bb719f9c1dfeaf1fb93a36a09b0cdd
Cassiano Bitencourt <tricecautopo@gmail.com>
2021-01-21 21:03:40 -0300

    Like

--- app/views/like_views.py
+++ app/views/like_views.py
@@ -18,28 +18,25 @@ def give_like():
 
     owner = Owner.query.get(get_jwt_identity())
 
-    list_of_dogs_in_owner = [dog.id for dog in owner.dogs]
+    if data['dog_id_give'] in [dog.id for dog in owner.dogs]:
+        if Like.query.filter_by(
+            dog_id_give=data['dog_id_give'],
+            dog_id_receive=data['dog_id_receive']
+        ).first() is not None:
+            return {'msg': "Like alredy exists!"}, HTTPStatus.BAD_REQUEST
 
-    for dog_id in list_of_dogs_in_owner:
-        if dog_id == data['dog_id_give']:
-            if Like.query.filter_by(
-                dog_id_give=data['dog_id_give'],
-                dog_id_receive=data['dog_id_receive']
-            ).first() is not None:
-                return {'msg': "Like alredy exists!"}, HTTPStatus.BAD_REQUEST
+        like = Like(
+            dog_id_give=data['dog_id_give'],
+            dog_id_receive=data['dog_id_receive'],
+            dislike=data['dislike']
+        )
 
-            like = Like(
-                dog_id_give=data['dog_id_give'],
-                dog_id_receive=data['dog_id_receive'],
-                dislike=data['dislike']
-            )
+        verify_match(like)
 
-            verify_match(like)
-
-            db.session.add(like)
-            db.session.commit()
-            return {'data': {'match': LikeSchema().dump(like)}}, \
-                HTTPStatus.CREATED
+        db.session.add(like)
+        db.session.commit()
+        return {'data': {'match': LikeSchema().dump(like)}}, \
+            HTTPStatus.CREATED
 
     msg = f"Dog with id {data['dog_id_give']} not exists, or is not your dog!"
     return {'msg': msg}, HTTPStatus.NOT_FOUND
